7320e9e9b0ef1bf1bbec9c2ffa61c82dfe693963
WinsomeYuen <winsomeyuen@hotmail.com>
2017-11-18 02:10:34 +0000

    New Changes

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  template: `
+      <div [style]="getStyle()">
+  `
 })
 export class MapComponent implements OnInit {
+  showStyle = false;
   chosenCountry = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+	
+	getStyle() {
+    const profilePicUrl = 'assets/images/background.pngsome-remote-server-url.jpg';
+    const style = `background-image: url(${profilePicUrl})`;
+    return this.sanitizer.bypassSecurityTrustStyle(style);
   }
 
 }
